f4a0123fa24687589ee32b15a23b1af33e49a94f
Joni <joni.kaup@outlook.com>
2024-07-20 22:21:47 +0300

    Added fortune.py

---
+++ src/fortune.py
@@ -0,0 +1,8 @@
+# Fortunen algoritmin toteutus
+# Algoritmi saa syötteekseen jaettavan avaruuden dimensiot (Kartan korkeus ja leveys) ja
+# listan kohteiden koordinaateista joiden mukaan avaruus halutaan jakaa osiin. Algoritmi 
+# palauttaa sanakirjan jossa avaimina ovat kartan koordinaatit ja arvoina mihin sektoriin 
+# kyseinen koordinaatti kuuluu. 
+
+def Fortune(dimensions: tuple, sites: list):
+    pass
